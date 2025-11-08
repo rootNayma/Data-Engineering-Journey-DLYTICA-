ce8e44eae442785ee72397f2ca67a43594aa43db
Nyamasang Tamang <tamangnayma@gmail.com>
2025-11-08 09:46:48 +0545

    Update 2). Generating, inserting fake million data.sql

--- 1st Month/SQL/SQL_Optimization/1).  B- Tree Indexing/2). Generating, inserting fake million data.sql
+++ 1st Month/SQL/SQL_Optimization/1).  B- Tree Indexing/2). Generating, inserting fake million data.sql
@@ -10,9 +10,9 @@ INSERT INTO employees (
     country
 )
 SELECT 
-    'FirstName' || i,
-    'LastName' || i,
-    'employee' || i || '@company.com',
+    'FirstName' || i,  -----> what is i for,  i will create  1,2,3,4,5,6,7,8,9.....               
+    'LastName' || i,   -----> everything inside' ' this is a string,  and || this will just join the things which will be firstname+1 
+    'employee' || i || '@company.com',    -----> here it will be empolyee123@company.com
     (i % 50) + 1,  -- 50 departments
     30000 + (random() * 120000)::numeric(10,2),  -- Salary between 30k-150k
     CURRENT_DATE - (random() * 3650)::integer,  -- Random date within 10 years
